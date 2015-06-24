45810c652dce8945a6dfabfc8eb3fe188e2acc48
laras126 <laras126@gmail.com>
2015-06-24 12:24:41 -0400

    add acf stylesheet, customize site-wide settings admin

--- functions.php
+++ functions.php
@@ -17,6 +17,7 @@
 			add_filter('get_twig', array($this, 'add_to_twig'));
 			add_action('init', array($this, 'register_post_types'));
 			add_action('init', array($this, 'register_taxonomies'));
+			add_action('init', array($this, 'tsk_acf_utils'));
 			parent::__construct();
 		}
 
@@ -34,6 +35,10 @@
 
 		function register_menus() {
 			require('lib/menus.php');
+		}
+
+		function tsk_acf_utils() {
+			require('lib/acf-utils.php');
 		}
 
 		function add_to_context($context){
@@ -96,12 +101,6 @@
 	 *
 	 */
 
-	
-	// Add Sitewide Options Page
-	if( function_exists('acf_add_options_page') ) {	
-		acf_add_options_page('Site-wide Options');
-	}
-
 
 	// Change Title field placeholders for Custom Post Types
 	// (You'll need to register the types, of course)
@@ -141,19 +140,6 @@
 	// Remove inline gallery styles
 	add_filter( 'use_default_gallery_style', '__return_false' );
 
-
-	// Add a 'Very Simple' toolbar style for the WYSIWYG editor in ACF
-	// http://www.advancedcustomfields.com/resources/customize-the-wysiwyg-toolbars/
-	function tsk_acf_wysiwyg_toolbar( $toolbars ) {
-
-		$toolbars['Links Only'] = array();
-
-		// Only one row of buttons
-		$toolbars['Links Only'][1] = array('bold', 'link', 'unlink' );
-
-		return $toolbars;
-	}
-	add_filter( 'acf/fields/wysiwyg/toolbars' , 'tsk_acf_wysiwyg_toolbar'  );
 
 
 
---
+++ lib/acf-utils.php
@@ -0,0 +1,38 @@
+<?php
+
+	/* 	
+		Add a 'Links Only' toolbar style for the following fields:
+		- Text/Image Block: text_caption
+		- Site-wide Settings: site_callout_text, site_footer_credits
+
+		Credit: http://www.advancedcustomfields.com/resources/customize-the-wysiwyg-toolbars/
+	*/
+
+	function tsk_acf_wysiwyg_toolbar( $toolbars ) {
+
+		$toolbars['Links Only'] = array();
+
+		// Only one row of buttons
+		$toolbars['Links Only'][1] = array('bold', 'link', 'unlink' );
+
+		return $toolbars;
+	}
+	add_filter( 'acf/fields/wysiwyg/toolbars' , 'tsk_acf_wysiwyg_toolbar'  );
+
+
+	// Register the Site-wide Options Page
+	if( function_exists('acf_add_options_page') ) {	
+		acf_add_options_page('Site-wide Options');
+	}
+
+	// Add stylesheet with custom ACF styles
+	function tsk_acf_admin_head() {
+		?>
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/acf.css">
+		<?php
+	}
+	add_action('acf/input/admin_head', 'tsk_acf_admin_head');
+
+
+
+?>
