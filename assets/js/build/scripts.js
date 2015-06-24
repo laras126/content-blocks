5a110c4308c0c22899e4b58b1cc3837ef1e24144
laras126 <laras126@gmail.com>
2015-06-24 19:19:05 -0400

    start some styling, add markup to some blocks

--- assets/js/build/scripts.js
+++ assets/js/build/scripts.js
@@ -150,27 +150,6 @@ $(document).ready(function() {
 	// }); // END scroll
 
 
-
-
-
-
-	// ----
-	// Misc
-	// ----
-
-	// Hack to keep out widows
-	// http://css-tricks.com/preventing-widows-in-post-titles/
-   
-	$('.item-title, .section-title, .main p, .lead').each( function() {
-		var wordArray = $(this).text().split(" ");
-		if (wordArray.length > 3) {
-			wordArray[wordArray.length-2] += "&nbsp;" + wordArray[wordArray.length-1];
-			wordArray.pop();
-	    	$(this).html(wordArray.join(" "));
-	  	}
-	});
-
-
 });
 
 
