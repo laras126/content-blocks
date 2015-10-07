4cb75614f0a03bf2c068b77abe78c16d71b2ce02
laras126 <laras126@gmail.com>
2015-10-06 20:47:47 -0400

    add yld custom types

--- lib/custom-types.php
+++ lib/custom-types.php
@@ -1,37 +1,140 @@
 <?php 
 
 	// ----
-	// Custom Type Example
+	// Pieces Custom Type
 	// ----
 
 	$labels = array(
-		'name'                => _x( 'Chapter', 'Post Type General Name', 'tsk' ),
-		'singular_name'       => _x( 'Chapter', 'Post Type Singular Name', 'tsk' ),
-		'menu_name'           => __( 'Chapters', 'tsk' ),
-		'name_admin_bar'      => __( 'Chapter', 'tsk' ),
-		'parent_item_colon'   => __( 'Parent Chapter:', 'tsk' ),
-		'all_items'           => __( 'All Chapters', 'tsk' ),
-		'add_new_item'        => __( 'Add New Chapter', 'tsk' ),
-		'add_new'             => __( 'Add New', 'tsk' ),
-		'new_item'            => __( 'New Chapter', 'tsk' ),
-		'edit_item'           => __( 'Edit Chapter', 'tsk' ),
-		'update_item'         => __( 'Update Chapter', 'tsk' ),
-		'view_item'           => __( 'View Chapter', 'tsk' ),
-		'search_items'        => __( 'Search Chapter', 'tsk' ),
-		'not_found'           => __( 'Not found', 'tsk' ),
-		'not_found_in_trash'  => __( 'Not found in Trash', 'tsk' ),
+		'name'                => _x( 'Collection', 'Post Type General Name', 'yld' ),
+		'singular_name'       => _x( 'Collection', 'Post Type Singular Name', 'yld' ),
+		'menu_name'           => __( 'Collections', 'yld' ),
+		'name_admin_bar'      => __( 'Collection', 'yld' ),
+		'parent_item_colon'   => __( 'Parent Collection:', 'yld' ),
+		'all_items'           => __( 'All Collections', 'yld' ),
+		'add_new_item'        => __( 'Add New Collection', 'yld' ),
+		'add_new'             => __( 'Add New', 'yld' ),
+		'new_item'            => __( 'New Collection', 'yld' ),
+		'edit_item'           => __( 'Edit Collection', 'yld' ),
+		'update_item'         => __( 'Update Collection', 'yld' ),
+		'view_item'           => __( 'View Collection', 'yld' ),
+		'search_items'        => __( 'Search Collection', 'yld' ),
+		'not_found'           => __( 'Not found', 'yld' ),
+		'not_found_in_trash'  => __( 'Not found in Trash', 'yld' ),
 	);
 	$rewrite = array(
-		'slug'                => 'chapter',
+		'slug'                => 'collection',
 		'with_front'          => true,
 		'pages'               => true,
 		'feeds'               => true,
 	);
 	$args = array(
-		'label'               => __( 'chapter', 'tsk' ),
-		'description'         => __( 'The Chapter post type.', 'tsk' ),
+		'label'               => __( 'collection_type', 'yld' ),
+		'description'         => __( 'The Collection post type.', 'yld' ),
 		'labels'              => $labels,
-		'supports'            => array( 'title', 'excerpt', 'thumbnail'),
+		'supports'            => array( 'title', 'excerpt', 'thumbnail', 'revisions' ),
+		'taxonomies'          => array( 'category', 'post_tag' ),
+		'hierarchical'        => false,
+		'public'              => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'menu_position'       => 5,
+		'menu_icon'           => 'dashicons-format-gallery',
+		'show_in_admin_bar'   => true,
+		'show_in_nav_menus'   => true,
+		'can_export'          => true,
+		'has_archive'         => true,		
+		'exclude_from_search' => false,
+		'publicly_queryable'  => true,
+		'rewrite'             => $rewrite,
+		'capability_type'     => 'page',
+	);
+	// register_post_type( 'collection_type', $args );
+ 
+
+
+	// ----
+	// Pieces Custom Type
+	// ----
+
+	$labels = array(
+		'name'                => _x( 'Piece', 'Post Type General Name', 'yld' ),
+		'singular_name'       => _x( 'Piece', 'Post Type Singular Name', 'yld' ),
+		'menu_name'           => __( 'Pieces', 'yld' ),
+		'name_admin_bar'      => __( 'Piece', 'yld' ),
+		'parent_item_colon'   => __( 'Parent Piece:', 'yld' ),
+		'all_items'           => __( 'All Pieces', 'yld' ),
+		'add_new_item'        => __( 'Add New Piece', 'yld' ),
+		'add_new'             => __( 'Add New', 'yld' ),
+		'new_item'            => __( 'New Piece', 'yld' ),
+		'edit_item'           => __( 'Edit Piece', 'yld' ),
+		'update_item'         => __( 'Update Piece', 'yld' ),
+		'view_item'           => __( 'View Piece', 'yld' ),
+		'search_items'        => __( 'Search Piece', 'yld' ),
+		'not_found'           => __( 'Not found', 'yld' ),
+		'not_found_in_trash'  => __( 'Not found in Trash', 'yld' ),
+	);
+	$rewrite = array(
+		'slug'                => 'piece',
+		'with_front'          => true,
+		'pages'               => true,
+		'feeds'               => true,
+	);
+	$args = array(
+		'label'               => __( 'piece', 'yld' ),
+		'description'         => __( 'The Piece post type.', 'yld' ),
+		'labels'              => $labels,
+		'supports'            => array( 'title', 'excerpt', 'thumbnail', 'revisions' ),
+		'taxonomies'          => array( 'category', 'post_tag', 'collection' ),
+		'hierarchical'        => false,
+		'public'              => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'menu_position'       => 5,
+		'menu_icon'           => 'dashicons-format-image',
+		'show_in_admin_bar'   => true,
+		'show_in_nav_menus'   => true,
+		'can_export'          => true,
+		'has_archive'         => true,		
+		'exclude_from_search' => false,
+		'publicly_queryable'  => true,
+		'rewrite'             => $rewrite,
+		'capability_type'     => 'post',
+	);
+	register_post_type( 'piece', $args );
+ 
+
+	// ----
+	// Projects Custom Type
+	// ----
+
+	$labels = array(
+		'name'                => _x( 'Project', 'Post Type General Name', 'yld' ),
+		'singular_name'       => _x( 'Project', 'Post Type Singular Name', 'yld' ),
+		'menu_name'           => __( 'Projects', 'yld' ),
+		'name_admin_bar'      => __( 'Project', 'yld' ),
+		'parent_item_colon'   => __( 'Parent Project:', 'yld' ),
+		'all_items'           => __( 'All Projects', 'yld' ),
+		'add_new_item'        => __( 'Add New Project', 'yld' ),
+		'add_new'             => __( 'Add New', 'yld' ),
+		'new_item'            => __( 'New Project', 'yld' ),
+		'edit_item'           => __( 'Edit Project', 'yld' ),
+		'update_item'         => __( 'Update Project', 'yld' ),
+		'view_item'           => __( 'View Project', 'yld' ),
+		'search_items'        => __( 'Search Project', 'yld' ),
+		'not_found'           => __( 'Not found', 'yld' ),
+		'not_found_in_trash'  => __( 'Not found in Trash', 'yld' ),
+	);
+	$rewrite = array(
+		'slug'                => 'project',
+		'with_front'          => true,
+		'pages'               => true,
+		'feeds'               => true,
+	);
+	$args = array(
+		'label'               => __( 'project', 'yld' ),
+		'description'         => __( 'The Project post type.', 'yld' ),
+		'labels'              => $labels,
+		'supports'            => array( 'title', 'excerpt', 'thumbnail', 'revisions'),
 		'taxonomies'          => array( 'category', 'post_tag' ),
 		'hierarchical'        => false,
 		'public'              => true,
@@ -46,9 +149,10 @@
 		'exclude_from_search' => false,
 		'publicly_queryable'  => true,
 		'rewrite'             => $rewrite,
-		'capability_type'     => 'page',
+		'capability_type'     => 'post',
 	);
-	register_post_type( 'chapter', $args );
+	register_post_type( 'project', $args );
  
+
 
  ?>
